a0f2c4bac08bbad03047f2de6720937c7b6c94d8
Raghav Jindal <raghavjindal131@gmail.com>
2020-07-29 16:13:55 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, {Fragment} from 'react';
+import './App.scss';
+import { BrowserRouter, Route } from "react-router-dom";
+import FilterMenu from './components/filter/filterMenu';
+
+
+function App() { 
+
+  return (
+    <BrowserRouter>
+    
+      
+      <div className="App">
+        <Route path="/" render={props => (
+          <Fragment>
+            <FilterMenu {...props} />
+             
+          </Fragment>
+        )} />
+      
+        
+      </div>
+      </BrowserRouter>
+  );
+}
+
+export default App;
